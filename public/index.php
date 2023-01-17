00498740ccbebbfdb1aa82c73e7a046157927cf9
Carlos Ferreira <carlos@especializati.com.br>
2023-01-17 10:51:32 -0300

    feat: show most popular usernames and passwords

--- public/index.php
+++ public/index.php
@@ -1,8 +1,24 @@
 <?php
 
-use Src\Example;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$example = new Example;
-echo $example->say();
+$usernames = fopen('top-usernames-shortlist.txt', 'r');
+
+while(!feof($usernames)) {
+    $username = fgets($usernames);
+    echo $username."<br>";
+}
+
+fclose($usernames);
+
+echo '<br>--------------------------------------------------------<br>';
+
+$passwords = fopen('2020-200_most_used_passwords.txt', 'r');
+
+while(!feof($passwords)) {
+    $password = fgets($passwords);
+    echo $password."<br>";
+}
+
+fclose($passwords);
+
