370bf6e9507ebf7b80b0fe4dce478a74d92cdb26
Carlos Ferreira <carlos@especializati.com.br>
2023-01-17 10:44:28 -0300

    chore: config psr-4

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo 'Hello World';
+use Src\Example;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$example = new Example;
+echo $example->say();
---
+++ src/Example.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Src;
+
+class Example
+{
+    public function say(): string
+    {
+        return 'Hello';
+    }
+}
